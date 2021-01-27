2fa7f49c3d8ac6082671684a4380f55e0c5b4133
서지상 <inaki96@naver.com>
2021-01-27 17:20:37 +0900

    testing responce on 2.4

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 // require ==> node module을 어디선가 가져오는것 ,express란 폴더에서 찾을거임 
 const app = express()
-const port = 4000
+const port = 3000
 const handleListening = () => {
 
-    console.log("Listening on: http://localhost:4000")
+    console.log(`Listening on: http://localhost:${port}"`)
 
 }
+
+const handleHome = (req, res) => {
+    console.log('Hi from Home!')
+
+    console.log(req ,res);
+
+}
+// problem, there must have responce, else infinite loading... etc).. ok ,error, http message ... 
+
+
+
+
+
+
+
+app.get("/",handleHome)
+//in get method, usually calls two object, request, responce  == who and what kind of data sent,,  
 app.listen(port, handleListening)
