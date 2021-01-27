4934d0a8c1e0e3b87169ade686d417a6ead2660d
서지상 <inaki96@naver.com>
2021-01-27 17:09:05 +0900

    add start script, test listening

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 // require ==> node module을 어디선가 가져오는것 ,express란 폴더에서 찾을거임 
 const app = express()
-const port = 3000
+const port = 4000
+const handleListening = () => {
 
+    console.log("Listening on: http://localhost:4000")
 
-app.listen(port)
+}
+app.listen(port, handleListening)
