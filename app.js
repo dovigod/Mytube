aafa34ce03cbab8b4d9f5e52810dd847720ec2a5
서지상 <inaki96@naver.com>
2021-02-06 17:42:00 +0900

    webPack error fixed

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ app.set('view engine', 'pug');
 app.set('views', './view');
 
 app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static('static'));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, '/static')));
+//app.use(express.static(path.join(__dirname, 'static')));
+
 app.use(logger('dev'));
 //app.use(express.static(`${__dirname}/static`));
 //app.use('static/main.css', express.static('main'));
