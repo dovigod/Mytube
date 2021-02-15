d79304a6659af60c7f53db432ff5323451feee92
서지상 <inaki96@naver.com>
2021-02-15 15:43:50 +0900

    blah blah

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,70 +1,62 @@
+import routes from '../routes';
+import User from '../models/user';
 
-import routes from "../routes";
+export const getJoin = (req, res) => {
+	res.render('join', {
+		pageTitle: 'Join'
+	});
+};
 
+export const postJoin = async (req, res) => {
+	const { name, email, password } = req.body;
 
-export const getJoin = (req , res) => {
-    res.render("join", {
-        pageTitle : "Join",
+	if (password[0] !== password[1]) {
+		res.status(400);
+		res.render('join', { pageTitle: 'join' });
+		//if wrong with verification,, make status code 400 bad request
+	} else {
+		try {
+			const user = await User({
+				name,
+				email
+			});
+			await User.register(user, password);
+		} catch (e) {
+			console.log(e);
+			res.redirect(routes.home);
+		}
+		res.redirect(routes.home);
+	}
 
-    
-})}
+	res.render('join', {
+		pageTitle: 'Join',
+		name,
+		password1: password[0],
+		email,
+		password2: password[1]
+	});
+};
 
-export const postJoin = (req , res) => {
-
-    const {name,email,password} = req.body;
-
-    if( password[0] !== password[1]){
-        res.status(400);
-        res.render("join",{ pageTitle: "join"});
-        //if wrong with verification,, make status code 400 bad request
-    }
-    else{
-        // to do :: register user
-        //to dp :: log user in
-        res.redirect(routes.home);
-    }
-
-    res.render("join", {
-        pageTitle : "Join",
-        name,
-        password1: password[0],
-        email,
-        password2: password[1],
-        
-    })
-
-}
-
-
-
-
-
-export const getLogin = (req , res) => {
-
-    res.render("login" , { 
-    pageTitle : "Log In",
-}
-)
-}
-
+export const getLogin = (req, res) => {
+	res.render('login', {
+		pageTitle: 'Log In'
+	});
+};
 
 export const postLogin = (req, res) => {
+	//    const { body :{ email , password}} = req;
+	// will compare with DB
 
-//    const { body :{ email , password}} = req;
-// will compare with DB
-    
+	res.redirect(routes.home);
+};
 
-    res.redirect(routes.home);
-}
+export const logout = (req, res) => res.redirect(routes.home);
 
-export const logout = (req , res) => res.redirect(routes.home);
-
-export const changePassword = (req, res) => res.render("changePassword");
+export const changePassword = (req, res) => res.render('changePassword');
 
 export const editProfile = (req, res) => {
-    res.render("editProfile", {
-        pageTitle : "Edit Profile"
-    });
-}
-export const userDetail = (req, res) => res.render("userDetail");
-
+	res.render('editProfile', {
+		pageTitle: 'Edit Profile'
+	});
+};
+export const userDetail = (req, res) => res.render('userDetail');
--- passport.js
+++ passport.js
@@ -1,5 +1,8 @@
-import passport from './passport';
-import user from './model/user';
+import passport from 'passport';
+import user from './models/user';
 
 passport.use(user.createStrategy());
 // email 과 password를 쓰는 strategy를 쓸꺼임
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
